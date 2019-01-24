f3189eb3f0e06cc3ca6e7e9297458af8808ec7cd
Claudiomar Araújo <claudiomar.araujo@lavid.ufpb.br>
2019-01-24 17:12:21 -0300

    API para autenticação

---
+++ src/main/java/com/example/selecaojava/payload/SignInRequest.java
@@ -0,0 +1,21 @@
+package com.example.selecaojava.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SignInRequest {
+
+    @Email
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+}
