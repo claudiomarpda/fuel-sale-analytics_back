03fecc1fad4d61a4242d93acdd836488073eb06f
Claudiomar Araújo <claudiomar.araujo@lavid.ufpb.br>
2019-01-24 10:10:37 -0300

    Projeto Spring Boot inicial com Web, JPA, H2 e Lombok

---
+++ src/main/java/com/example/selecaojava/SelecaoJavaApplication.java
@@ -0,0 +1,14 @@
+package com.example.selecaojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelecaoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelecaoJavaApplication.class, args);
+	}
+
+}
+
